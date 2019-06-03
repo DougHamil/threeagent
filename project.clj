6adc0cdf6dc364e82eb4a960f87869797dca313c
Doug Hamilton <doug.hamil@gmail.com>
2019-06-03 13:46:41 -0600

    Added stuff

--- project.clj
+++ project.clj
@@ -24,7 +24,8 @@
 
   :npm {:dependencies [[three "0.100.0"]]}
 
-  :doo {:build "test"}
+  :doo {:paths {:karma "node_modules/.bin/karma"
+                :phantom "node_modules\\.bin\\phantom"}}
 
   :figwheel {:http-server-root "public"
              :nrepl-port 7889
