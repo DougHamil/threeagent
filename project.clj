1c7fee6d257f6c147b90ccccfd21df84848ffd15
Doug Hamilton <doug.hamil@gmail.com>
2020-01-28 19:25:02 -0700

    Version 0.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject doughamil/threeagent "0.0.2-SNAPSHOT"
+(defproject doughamil/threeagent "0.0.2"
   :description "Build Three.js apps in a reagent-like fashion"
   :url "https://github.com/DougHamil/threeagent"
 
