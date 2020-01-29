cd101a90eeee3b62a1a55dc97c17016aff3edc75
Release Script <circleci@circleci>
2020-01-29 03:30:02 +0000

    Version 0.0.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject doughamil/threeagent "0.0.4-SNAPSHOT"
+(defproject doughamil/threeagent "0.0.4"
   :description "Build Three.js apps in a reagent-like fashion"
   :url "https://github.com/DougHamil/threeagent"
   :license {:name "MIT"}
