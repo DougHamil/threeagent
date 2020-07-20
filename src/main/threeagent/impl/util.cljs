9243627c58b124f4604535416eb4d328a3887e79
Doug Hamilton <doug.hamil@gmail.com>
2020-07-19 20:07:28 -0600

    Docs as root

--- src/main/threeagent/impl/util.cljs
+++
@@ -1,7 +0,0 @@
-(ns threeagent.impl.util)
-
-(defn log [m] (.log js/console m))
-
-(defonce pi-times-2 (* js/Math.PI 2.0))
-(defonce pi js/Math.PI)
-(defonce pi-over-2 (/ js/Math.PI 2.0))
--- src/test/threeagent/ratom_test.cljs
+++
@@ -1,9 +0,0 @@
-(ns threeagent.ratom-test
-  (:require [threeagent.core :as th]
-            [cljs.test :refer-macros [deftest is testing]]))
-
-(deftest ratom-test
-  (testing "ratom is creatable"
-    (is (= 1 (:test @(th/atom {:test 1}))))))
-
-
