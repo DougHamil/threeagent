4e30f534b9b71a8fb116d192b1d708e91a2563a0
DougHamil <doug.hamil@gmail.com>
2025-12-26 14:12:03 -0700

    fix CI

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,28 @@ process.env.CHROME_BIN = require('puppeteer').executablePath()
 
 module.exports = function(config) {
     config.set({
-        browsers: ['ChromeHeadless'],
+        // 1. Switch to your custom CI launcher
+        browsers: ['ChromeHeadlessCI'],
+
+        // 2. Define the custom launcher with required flags for Docker
+        customLaunchers: {
+            ChromeHeadlessCI: {
+                base: 'ChromeHeadless',
+                flags: [
+                    '--no-sandbox',                // Required for Docker
+                    '--disable-gpu',               // Prevents the GPU crashes in your logs
+                    '--disable-dev-shm-usage',      // Prevents memory issues in CI
+                    '--disable-software-rasterizer',
+                    '--remote-debugging-port=9222'
+                ]
+            }
+        },
+
+        // 3. Increase timeouts for slower CI environments
+        browserNoActivityTimeout: 60000,
+        browserDisconnectTimeout: 20000,
+        captureTimeout: 60000,
+
         // The directory where the output file lives
         basePath: 'target',
         // The file itself
